78c696dd9f37c739ef4d8aff167d5157b14b7083
Steven Hynes <epicstevo555@gmail.com>
2021-04-26 10:14:50 -0400

    fixed indexing in voting systems

--- common/voting_systems.py
+++ common/voting_systems.py
@@ -10,7 +10,8 @@ class FirstPastThePostVotingSystem(VotingSystem):
                 count[candidate] = 0
             for voter in district.voters:
                 candidates = voter.candidate_retreiver.get_candidates(voter, district)
-                count[candidates[0]] += voter.vote_power
+                if candidates:
+                    count[candidates[0][0]] += voter.vote_power
             winner = max(count, key=lambda c: count[c])
             district.representatives = [winner]
             country.legislature.append(winner)
@@ -21,27 +22,30 @@ class RankedChoiceVotingSystem(VotingSystem):
     def election(self, country):
         country.legislature.clear()
         for district in country.districts:
-            total_vote_power = sum([v.vote_power for v in country.voters])
             vote_lists = []
             for voter in district.voters:
                 candidates = voter.candidate_retreiver.get_candidates(voter, district)
-                vote_lists.append([candidates, 0, voter.vote_power])
+                if candidates:
+                    vote_lists.append([candidates, 0, voter.vote_power])
             count = {}
+            for candidate in district.candidates:
+                count[candidate] = 0
             winner = None
             while True:
-                for candidate in district.candidates:
+                for candidate in count:
                     count[candidate] = 0
                 for vote_list in vote_lists:
                     if vote_list[1] < len(vote_list[0]):
-                        count[vote_list[0][vote_list[1]]] += vote_list[2]
+                        count[vote_list[0][vote_list[1]][0]] += vote_list[2]
                 winner = max(count, key=lambda c: count[c])
                 loser = min(count, key=lambda c: count[c])
-                if count[winner] > total_vote_power / 2:
+                vote_sum = sum(count[c] for c in count)
+                if count[winner] > vote_sum / 2:
                     break
                 else:
                     del count[loser]
                     for vote_list in vote_lists:
-                        while vote_list[1] < len(vote_list[0]) and vote_list[0][vote_list[1]] not in count:
+                        while vote_list[1] < len(vote_list[0]) and vote_list[0][vote_list[1]][0] not in count:
                             vote_list[1] += 1
             district.representatives = [winner]
             country.legislature.append(winner)
