e293326365055324e16ca73e035e77b60b6d49ef
Steven Hynes <epicstevo555@gmail.com>
2021-04-27 13:42:14 -0400

    added district representation method

--- common/base_modules/country.py
+++ common/base_modules/country.py
@@ -21,5 +21,17 @@ class Country:
             dist.district_updater.update(dist)
         self.country_updater.update(self)
         self.election_count += 1
-        
+
+    def get_district_representation(self): #For each voter in a district, add the squared distance to the closest representative
+        dist_list = []
+        for district in self.districts:
+            total_error = 0
+            for voter in district.voters:
+                total_distance = 0 #total distance to candidates
+                for rep in district.representatives:
+                    distance = voter.views.distance(rep.views_current)
+                    total_distance += distance
+                total_error += (total_distance / len(district.representatives)) ** 2 #avg distance to candidates
+            dist_list.append(total_error)
+        return dist_list
         
